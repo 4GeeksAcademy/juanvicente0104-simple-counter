36e3a56628dc873a88b3b20c8e79470a98652c16
Juan Guerrero <juanvicente0104@gmail.com>
2023-10-05 15:27:21 +0000

    project delivery

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,21 +4,17 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container vh-100 d-flex align-items-center justify-content-center">
+			<div className="row">
+				<h1 className="col-2 bg-dark text-white justify-content-center p-4"> <i class="far fa-clock"></i> </h1>
+				<h1 className="col-2 bg-dark text-white justify-content-center p-4"> {props.num5} </h1>
+				<h1 className="col-2 bg-dark text-white justify-content-center p-4">  {props.num4} </h1>
+				<h1 className="col-2 bg-dark text-white justify-content-center p-4">  {props.num3} </h1>
+				<h1 className="col-2 bg-dark text-white justify-content-center p-4">  {props.num2} </h1>
+				<h1 className="col-2 bg-dark text-white justify-content-center p-4">  {props.num1} </h1>
+			</div>
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,20 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+//Initialize our counter variable
+let counter = 0
+
+setInterval(() => {
+    let first = Math.floor((counter/1) % 10)
+    let second = Math.floor((counter/10) % 10)  
+    let third = Math.floor((counter/100) % 10)      
+    let fourth = Math.floor((counter/1000) % 10)  
+    let fifth = Math.floor((counter/10000) % 10)  
+    counter = counter + 1
+
+    //render your react application
+    ReactDOM.render(
+    <Home num1={first} num2={second} num3={third} num4={fourth} num5={fifth}/>, document.querySelector("#app"));
+    
+
+},1000)
